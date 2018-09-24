c91d21c04d6b6866a4055e410f792ab43315a88f
mahveenya <mahveenya.k.a@gmail.com>
2018-09-25 00:41:39 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+let arr = equation.split(' ');
+let a, b, c, d, x1, x2, result;
+
+  a = Number(arr[0]);
+  b = Number(arr[3] + arr[4]);
+  c = Number(arr[7] + arr[8]);
+  d = b*b - 4*a*c;
+
+  if (d > 0) {
+    x1 = (-b - Math.sqrt(d)) / (2*a);
+    x2 = (-b + Math.sqrt(d)) / (2*a);
+  } 
+  else if (d == 0) {
+    x1 = x2 = -b / (2*a);
+  } 
+  else {x1 = x2 = null}
+
+  result = [Math.round(x1), Math.round(x2)];
+  
+  let compare = (i, j) => i - j;
+  
+  result.sort(compare);
+
+  return result;
+
 }
